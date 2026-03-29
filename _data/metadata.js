1a1a9872e9d99737c578a19fcd9db69dd59b1167
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2026-03-29 12:28:26 +0200

    Fix typo in OG metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "DaughterDrivenDev",
 	url: "https://www.daughterdriven.dev/",
 	language: "en",
-	description: "Daughter Drivevn Development. Parents in Tech.",
+	description: "Daughter Driven Development. Parents in Tech.",
 	author: {
 		name: "Ola Gasidlo-Brändel, Marcin Skirzynski",
 		email: "parents@daughterdriven.dev",
